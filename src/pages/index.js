0cf393f6559772c63a22b6d2f7958ebc84680e16
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-03-25 20:26:12 -0500

    Updated index.js page

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,9 +12,9 @@ export default () => {
     <>
       <Helmet>
         <meta charSet="utf-8" />
-        <title>{title || 'Gatsby Simplefolio'}</title>
+        <title>{title || 'Chris Brown Web Dev'}</title>
         <html lang={lang || 'en'} />
-        <meta name="description" content={description || 'Gatsby Simplefolio'} />
+        <meta name="description" content={description || 'Front-end Web-developer'} />
       </Helmet>
       <App />
     </>
