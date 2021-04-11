91ca5fac2d354451d3153ac88a5f74a12274034d
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-04-11 11:07:34 -0500

    Added a bootstrap form to contact section, still styling

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from 'react';
 import Fade from 'react-reveal/Fade';
-import { Container } from 'react-bootstrap';
+import { Container, Form, Button } from 'react-bootstrap';
 import PortfolioContext from '../../context/context';
 import Title from '../Title/Title';
 
@@ -17,14 +17,32 @@ const Contact = () => {
             <p className="contact-wrapper__text">
               {cta || 'Would you like to work with me? Awesome!'}
             </p>
-            <a
-              target="_blank"
-              rel="noopener noreferrer"
-              className="cta-btn cta-btn--resume"
-              href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
-            >
-              {btn || "Let's Talk"}
-            </a>
+            <Form >
+              <Form.Group controlId="formBasicEmail" className="text-left">
+                <Form.Label >Name</Form.Label>
+                <Form.Control type="text" placeholder="Enter Name" />
+              </Form.Group>
+              <Form.Group controlId="formBasicEmail" className="text-left">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control type="email" placeholder="Enter email" />
+              </Form.Group>
+              <Form.Group controlId="formBasicPassword" className="text-left">
+                <Form.Label>Message</Form.Label>
+                <Form.Control type="text" placeholder="Password" />
+              </Form.Group>
+              {/* <Button variant="primary" type="submit">
+                Submit
+              </Button> */}
+              <a
+                target="_blank"
+                rel="noopener noreferrer"
+                className="cta-btn cta-btn--resume"
+                href={email ? `mailto:${email}` : 'https://github.com/cobidev/react-simplefolio'}
+              >
+                {btn || "Let's Talk"}
+              </a>
+            </Form>
+
           </div>
         </Fade>
       </Container>
