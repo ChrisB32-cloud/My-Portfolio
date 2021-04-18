4a3f7fed8dce3835c7a73021d48ebe02fd8f93d0
ChrisBrownTheGuy <cbrowndev88@gmail.com>
2021-04-18 18:21:03 -0500

    Made change to experience section for responsivness

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -26,7 +26,7 @@ const Projects = () => {
     <section id="projects">
       <Container>
         <div className="project-wrapper">
-          <Title title="Experience/Projects" />
+          <Title title="Experience" />
           {projects.map((project) => {
             const { title, info, info2, url, repo, img, id } = project;
 
